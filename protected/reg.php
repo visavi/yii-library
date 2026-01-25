a6d0f5e27d3b8372ce62e9945091587186151a00
visavi <visavi.net@mail.ru>
2026-01-25 20:37:05 +0300

    Удалил лишние файлы

--- protected/reg.php
+++
@@ -1,8 +0,0 @@
-<?php
-$yii=dirname(__FILE__).'/../framework/yii.php';
-$user = new User();
-$user->username = 'admin';
-$user->password = CPasswordHelper::hashPassword('secret');
-$user->email = 'test@example.com';
-$user->created_at = time();
-$user->save();
